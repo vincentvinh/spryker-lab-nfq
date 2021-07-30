eaa4c5872f0f80a5cf768c10ae1b71e6f28c9add
Hieu Le <hieu.le@nfq.asia>
2021-07-30 22:05:07 +0700

    Enable deployment workflow (#3)
    * Use docker/composer base image
    
    * Use new runner
    
    * Export images with git sha as tag
    
    * Add push images job to workflow
    
    * Repace the image version during exporting images
    
    * Revert back to the 1.0 tag because of the docker image cache
    
    * Enable kubectl and helm
    
    * Write kubeconfig
    
    * Use pod service account
    
    * Update chart dependencies before upgrade
    
    * Use bitnami repo URL
    
    * Disable service account of rabbitmq
    
    * Support deploy the correct container verion on release
    
    * Update staging config to use DE and VN stores

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,33 @@ set -exuo pipefail
 KUBE_NAMESPACE="beta-shop"
 STORES=(DE AT)
 CLI_POD=$(kubectl -n ${KUBE_NAMESPACE} get pod --selector component=spryker-cli --field-selector=status.phase=Running -o name)
+DOCKER_IMAGE_VERSION="1.0"
+
+while getopts "t:" opt; do
+    case "${opt}" in
+        t)
+            DOCKER_IMAGE_VERSION=${OPTARG}
+            ;;
+        # Unknown option specified
+        \?)
+            printf "\nUnknown option -%s is acquired.\n" ${OPTARG}
+            exit 1
+            ;;
+        *)
+            echo ${opt}
+            ;;
+    esac
+done
 
 for STORE in "${STORES[@]}"
 do
     kubectl -n ${KUBE_NAMESPACE} exec "${CLI_POD}" -- bash -c "APPLICATION_STORE=${STORE} vendor/bin/console scheduler:suspend"
 done
 
-helm --namespace ${KUBE_NAMESPACE} upgrade --install spryker kubernetes/spryker --wait
+helm dependency update kubernetes/spryker
+helm --namespace ${KUBE_NAMESPACE} upgrade --install spryker kubernetes/spryker \
+    --set container.version="${DOCKER_IMAGE_VERSION}" \
+    --wait
 kubectl -n ${KUBE_NAMESPACE} rollout status -w deployment/cli
 kubectl -n ${KUBE_NAMESPACE} rollout status -w deployment/scheduler
 until [ $(kubectl -n ${KUBE_NAMESPACE} get pod --selector component=spryker-cli --field-selector=status.phase=Running -o name | wc -l) -eq 1 -a $(kubectl -n ${KUBE_NAMESPACE} get pod --selector component=spryker-scheduler --field-selector=status.phase=Running -o name | wc -l) -eq 1 ];
--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -2,12 +2,29 @@
 set -exuo pipefail
 
 APP_IMAGE_NAME_NAMESPACE=beta-shop
-DOCKER_IMAGE_VERSION=1.0
+BUILT_DOCKER_IMAGE_VERSION=1.0
+DOCKER_IMAGE_VERSION=$BUILT_DOCKER_IMAGE_VERSION
 INTERNAL_CONTAINER_REGISTRY=registry.k8s.spryker-solution.nfq.internal
+
+while getopts "t:" opt; do
+    case "${opt}" in
+        t)
+            DOCKER_IMAGE_VERSION=${OPTARG}
+            ;;
+        # Unknown option specified
+        \?)
+            printf "\nUnknown option -%s is acquired.\n" ${OPTARG}
+            exit 1
+            ;;
+        *)
+            echo ${opt}
+            ;;
+    esac
+done
 
 for IMAGE_SUFFIX in app frontend cli
 do
     DOCKER_IMAGE=${APP_IMAGE_NAME_NAMESPACE}_${IMAGE_SUFFIX}
-    docker image tag ${DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} ${INTERNAL_CONTAINER_REGISTRY}/${DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION}
+    docker image tag ${DOCKER_IMAGE}:${BUILT_DOCKER_IMAGE_VERSION} ${INTERNAL_CONTAINER_REGISTRY}/${DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION}
     docker image push --all-tags ${INTERNAL_CONTAINER_REGISTRY}/${DOCKER_IMAGE}
 done
