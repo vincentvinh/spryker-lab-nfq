8a68b92bed460ce8a752f5678fad9e980bbbd550
An Nguyen <an.nguyen@nfq.asia>
2021-07-30 09:41:55 +0700

    [ST-27] Fix code style.

--- src/Pyz/Yves/Router/RouterConfig.php
+++ src/Pyz/Yves/Router/RouterConfig.php
@@ -1,31 +1,36 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Pyz\Yves\Router;
 
-use Spryker\Yves\Router\RouterConfig AS RouterConfigSpryker;
+use Spryker\Yves\Router\RouterConfig as RouterConfigSpryker;
 
 class RouterConfig extends RouterConfigSpryker
 {
     /**
      * @return string[]
      */
-    public function getAllowedLanguages() : array
+    public function getAllowedLanguages(): array
     {
         return [
             'de',
             'en',
-            'vi'
+            'vi',
         ];
     }
 
     /**
      * @return string[]
      */
-    public function getAllowedStores() :array
+    public function getAllowedStores(): array
     {
         return [
           'DE',
-          'VN'
+          'VN',
         ];
     }
 }
