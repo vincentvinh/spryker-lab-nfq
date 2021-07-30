87a25acfd9f812b6ae506b4324ea15953a2fa971
Tai Tran <tai.tranngoc@kaplan.com>
2021-07-30 15:05:32 +0700

    remove redundant code

--- src/Pyz/Zed/PriceProductStorage/PriceProductStorageDependencyProvider.php
+++ src/Pyz/Zed/PriceProductStorage/PriceProductStorageDependencyProvider.php
@@ -7,7 +7,6 @@
 
 namespace Pyz\Zed\PriceProductStorage;
 
-use Orm\Zed\PriceProduct\Persistence\SpyPriceProductStoreQuery;
 use Spryker\Zed\AvailabilityGui\Dependency\Facade\AvailabilityToStoreFacadeBridge;
 use Spryker\Zed\Kernel\Container;
 use Spryker\Zed\PriceProductStorage\PriceProductStorageDependencyProvider as SprykerPriceProductStorageDependencyProvider;
@@ -41,37 +40,11 @@ class PriceProductStorageDependencyProvider extends SprykerPriceProductStorageDe
      *
      * @return \Spryker\Zed\Kernel\Container
      */
-    public function providePersistenceLayerDependencies(Container $container)
-    {
-        $container = $this->addPriceProductStoreQuery($container);
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
     public function addStoreFacade(Container $container)
     {
         $container->set(static::FACADE_STORE, function (Container $container) {
             return new AvailabilityToStoreFacadeBridge($container->getLocator()->store()->facade());
         });
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    protected function addPriceProductStoreQuery(Container $container): Container
-    {
-        $container->set(static::PROPEL_QUERY_PRICE_PRODUCT_STORE, $container->factory(function () {
-            return SpyPriceProductStoreQuery::create();
-        }));
 
         return $container;
     }
