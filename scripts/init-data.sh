6ad80d0be10e2da50c6b47f57329a0c5363d8ccc
Hieu Le <letrunghieu.cse09@gmail.com>
2021-07-31 10:29:37 +0700

    Update the init script and remember to setup jenkins

--- scripts/init-data.sh
+++ scripts/init-data.sh
@@ -7,4 +7,4 @@ STORES=(DE VN)
 CLI_POD=$(kubectl -n ${KUBE_NAMESPACE} get pod --selector component=spryker-cli --field-selector=status.phase=Running -o name)
 
 STORE="${STORES[0]}"
-kubectl -n ${KUBE_NAMESPACE} exec "${CLI_POD}" -- bash -c "APPLICATION_STORE=${STORE} vendor/bin/install -r ${SPRYKER_PIPELINE} -s init-storage -s init-storages-per-region -s demodata"
+kubectl -n ${KUBE_NAMESPACE} exec "${CLI_POD}" -- bash -c "APPLICATION_STORE=${STORE} vendor/bin/install -r ${SPRYKER_PIPELINE} -s init-storage -s init-storages-per-region -s demodata -s scheduler-start"
